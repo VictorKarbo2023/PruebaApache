6c0066b98046a134470e7ed3c48228c619ef81d2
VictorKarbo2023 <victoreoijapones@gmail.com>
2023-12-06 17:13:59 +0100

    Proyecto de Apache Netbeans

---
+++ src/main/java/com/mycompany/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagithub;
+
+/**
+ *
+ * @author VSPC
+ */
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
